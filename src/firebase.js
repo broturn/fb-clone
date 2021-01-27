e951d52a2037f2de3b06312794729fc6bb46dffc
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-01-26 20:41:56 -0600

    added firebase config into the project

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC1eDf85nHOblgUC4nwvyb1ZxAxUw0-THs",
+    authDomain: "fb-clone-a25e9.firebaseapp.com",
+    projectId: "fb-clone-a25e9",
+    storageBucket: "fb-clone-a25e9.appspot.com",
+    messagingSenderId: "486794761901",
+    appId: "1:486794761901:web:d8438fcca01340ac481824",
+    measurementId: "G-8PNMTJEM16"
+  };
