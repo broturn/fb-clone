1b640dc0ac8d5221d4933d64abd225d7b9d4c072
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-01-27 21:42:03 -0600

    Starting building story and storyreel

---
+++ src/StoryReel.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Story from './Story';
+import "./StoryReel.css";
+
+function StoryReel() {
+    return (
+        <div className="storyReel">
+
+            <Story 
+                image="https://mkolaterblogouwugirl.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
+                profileSrc="https://avatars2.githubusercontent.com/u/24712956?s=400&u=n71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
+                tile="Sonny Sangha"
+            />
+            {/* Story */}
+            {/* Story */}
+            {/* Story */}
+            {/* Story */}
+            {/* Story */}
+        </div>
+    )
+}
+
+export default StoryReel
